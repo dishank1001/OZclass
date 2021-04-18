15a541c2dac3212b9429ec5659957cd513d1d282
dishank1001 <agrdsk5@gmail.com>
2021-04-18 15:52:51 +0530

    error handling of routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import products from './data/products.js';
 import connectDB from './config/db.js';
 import productPath from './routes/productPath.js'
@@ -16,6 +17,9 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products', productPath)
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
